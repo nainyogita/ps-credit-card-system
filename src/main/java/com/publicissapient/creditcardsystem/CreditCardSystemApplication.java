1a3c19c2ca89ff3493cb290c72f595007f90bc18
Yogita <nain.yogita@gmail.com>
2021-06-16 07:32:02 +0100

    Initial commit

---
+++ src/main/java/com/publicissapient/creditcardsystem/CreditCardSystemApplication.java
@@ -0,0 +1,13 @@
+package com.publicissapient.creditcardsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardSystemApplication.class, args);
+	}
+
+}
